a75ffc637b51b689f9a9c0ee8186858b6ace3f9e
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-10 15:59:35 +0900

    test

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -31,12 +31,13 @@ public class RiotApiController {
     @Value("${riot.api.key}")
     private String riotApiKey;
 
-    @RequestMapping(value = "/summoner/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/summoner/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Summoner querySummoner(@PathVariable("name") String summonerName) throws UnsupportedEncodingException {
         final String url = riotApiEndpoint + "/summoner/by-name/" +
                 summonerName +
                 "?api_key=" +
                 riotApiKey;
+
         String response = restTemplate.getForObject(url, String.class);
         Map<String, Object> parsedMap = new JacksonJsonParser().parseMap(response);
 
@@ -45,10 +46,8 @@ public class RiotApiController {
         Map<String, Object> summonerDetail = (Map<String, Object>) parsedMap.values().toArray()[0];
         String queriedName = (String)summonerDetail.get("name");
         int queriedLevel = (Integer)summonerDetail.get("summonerLevel");
-        CalcApp app = new CalcApp(queriedName);
-        int result = (int)app.getResult();
-        Summoner summoner = new Summoner(queriedName, result);
-        
+        Summoner summoner = new Summoner(queriedName, queriedLevel);
+
         return summoner;
     }
 }
