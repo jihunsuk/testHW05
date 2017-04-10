7694676754e260bc714a87178780292f7b8d6b7e
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-10 16:56:49 +0900

    test

--- src/main/java/univ/lecture/riotapi/model/Summoner.java
+++ src/main/java/univ/lecture/riotapi/model/Summoner.java
@@ -8,15 +8,10 @@ import lombok.NoArgsConstructor;
  * Created by tchi on 2017. 4. 1..
  */
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class Summoner {
     private String name;
     private int summonerLevel;
     private int result;
-    public Summoner(String name, int summonerLevel, int result){
-    	this.name = name;
-    	this.summonerLevel= summonerLevel;
-    	this.result = result;
-    }
 }
