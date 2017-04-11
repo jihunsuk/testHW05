d6c85e90775da258374b761e93d339c7fd9a9a45
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-11 16:29:29 +0900

    change value to'/calc/{name}', postForObject

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -34,7 +34,7 @@ public class RiotApiController {
     @Value("${riot.api.key}")
     private String riotApiKey;
 
-    @RequestMapping(value = "/summoner/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/calc/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public JSONResult queryResult(@PathVariable("name") String expression) throws UnsupportedEncodingException {
         final String url = riotApiEndpoint + "/summoner/by-name/" +
         		expression +
@@ -43,13 +43,12 @@ public class RiotApiController {
         final int teamId = 8; //조번호(8조) 
         double mathResult;
         
-        String response = restTemplate.getForObject(url, String.class);
-        Map<String, Object> parsedMap = new JacksonJsonParser().parseMap(response);
-        parsedMap.forEach((key, value) -> log.info(String.format("key [%s] type [%s] value [%s]", key, value.getClass(), value)));
-        Map<String, Object> summonerDetail = (Map<String, Object>) parsedMap.values().toArray()[0];
-        String queriedName = (String)summonerDetail.get("msg");
+        String response = restTemplate.postForObject(url, null, String.class);
+        //Map<String, Object> parsedMap = new JacksonJsonParser().parseMap(response);
+        //parsedMap.forEach((key, value) -> log.info(String.format("key [%s] type [%s] value [%s]", key, value.getClass(), value)));
+        //Map<String, Object> summonerDetail = (Map<String, Object>) parsedMap.values().toArray()[0];
+        //String queriedName = (String)summonerDetail.get("msg");
         //int queriedLevel = (Integer)summonerDetail.get("summonerLevel");
-        
         
         /* 수식을 계산 */
         CalcApp app = new CalcApp(expression);
