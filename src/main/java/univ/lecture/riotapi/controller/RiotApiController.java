67be8a02b5abda2d5499542b66000c3edb83c4c9
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-11 15:30:30 +0900

    change model name(Summoner to Result)

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import univ.lecture.riotapi.model.Summoner;
+import univ.lecture.riotapi.model.Result;
 
 import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
